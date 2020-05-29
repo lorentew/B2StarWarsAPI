c9d617ec32ec120970ad174140d840458a8544af
Willians Lorente <willore@gmail.com>
2020-05-29 13:52:27 -0300

    Criação do Projeto

---
+++ src/main/java/com/wlorente/starwarsapi/B2StarWarsApiApplication.java
@@ -0,0 +1,13 @@
+package com.wlorente.starwarsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2StarWarsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B2StarWarsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wlorente/starwarsapi/B2StarWarsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wlorente.starwarsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2StarWarsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
